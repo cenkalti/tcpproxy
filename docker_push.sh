130afee228fd1786db5a7a39d16395f2282c21a8
Cenk Alti <cenkalti@gmail.com>
2020-04-13 10:38:08 +0300

    also push as latest

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 docker build -t tcpproxy .
 docker tag tcpproxy $DOCKER_USERNAME/tcpproxy:${TRAVIS_TAG:1}
+docker tag tcpproxy $DOCKER_USERNAME/tcpproxy:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/tcpproxy:${TRAVIS_TAG:1}
+docker push $DOCKER_USERNAME/tcpproxy:latest
